f36d8f22093b622ad2230252d155bd93de652de5
sergey poludnevich <flagbz0@gmail.com>
2020-09-18 01:51:31 +0300

    final

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -14,7 +14,7 @@ namespace Factorial
         {
             Console.Write("Enter number to factorial it:");
             int num;
-            while (!int.TryParse(Console.ReadLine(), out num)) Console.WriteLine("Do not write chars, only int!");
+            while (!int.TryParse(Console.ReadLine(), out num)) Console.WriteLine("Do not write chars, only integers!");
             Console.WriteLine($"Factorial of {num}  =  {Factorial(num)}");
         }
     }
